812fe785a7087d3c605b3aa8fd3b5144b056f6c4
wachxra <wachirawadee.junt@bumail.net>
2025-04-24 23:25:27 +0700

    Finished Version
    - All physics can run
    - Create scene
    - Camera can follow player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    private Vector2 moveInput;
+
+    private float move;
+    [SerializeField] float speed;
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isJumping;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb2d.AddForce(moveInput * speed);
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+
+            Debug.Log("Jump");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+}
